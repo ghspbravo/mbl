f130d60f0a9516dac01dcc6d46c5d1c20d567606
ghspbravo <gmspbravo@gmail.com>
2020-04-30 03:35:29 +0500

    fix: limit photo height in list items components

--- components/Members/MemberItem.tsx
+++ components/Members/MemberItem.tsx
@@ -1,30 +1,39 @@
-import React, { ReactElement } from 'react'
-import Link from 'next/link'
-import Pages from '../../constants/pages'
-import { shortMember } from '../../constants/formatters/membersFormatter'
+import React, { ReactElement } from "react";
+import Link from "next/link";
+import Pages from "../../constants/pages";
+import { shortMember } from "../../constants/formatters/membersFormatter";
 
 interface Props {
-  contents: shortMember
+	contents: shortMember;
 }
 
 export default function MemberItem({ contents: member }: Props): ReactElement {
-  return (
-    <Link href={Pages.Members.route + `/${member.id}`}>
-      <div className="member-item">
-        <img className="responsive" src={member.photo} alt="Фотография участника" />
-        <div className="align-center mt-2">
-          <span className="member__name"><b>{member.name}</b></span>
-          <br /> <span className="member__role">{member.role}</span>
-        </div>
-        <style jsx>{`
-        .member-item {
-          cursor: pointer;
-        }
-        .member-item:hover .member__name {
-          text-decoration: underline;
-        }
-        `}</style>
-      </div>
-    </Link>
-  )
+	return (
+		<Link href={Pages.Members.route + `/${member.id}`}>
+			<div className="member-item">
+				<img
+					className="responsive member-photo"
+					src={member.photo}
+					alt="Фотография участника"
+				/>
+				<div className="align-center mt-2">
+					<span className="member__name">
+						<b>{member.name}</b>
+					</span>
+					<br /> <span className="member__role">{member.role}</span>
+				</div>
+				<style jsx>{`
+					.member-photo {
+						max-height: 250px;
+					}
+					.member-item {
+						cursor: pointer;
+					}
+					.member-item:hover .member__name {
+						text-decoration: underline;
+					}
+				`}</style>
+			</div>
+		</Link>
+	);
 }
