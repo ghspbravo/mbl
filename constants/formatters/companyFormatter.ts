4842c49fa7935bdbc92bd7402f2aa0158f5b035e
ghspbravo <gmspbravo@gmail.com>
2020-05-02 13:31:41 +0500

    feat(company): add portfolio and members

--- constants/formatters/companyFormatter.ts
+++ constants/formatters/companyFormatter.ts
@@ -1,11 +1,17 @@
 import Formatter from "./rootFormatter";
 import pass from "../../assets/pass.png";
 import { businessSizesList } from "../businessSize";
+import { shortMember } from "./membersFormatter";
 
 export interface shortCompany {
 	id: number;
 	photo: string;
 	shortTitle: string;
+}
+
+interface portfolioItem {
+	id: number;
+	title: string;
 }
 
 export interface Company {
@@ -19,6 +25,14 @@ export interface Company {
 
 	creator: string;
 	foundationDate: string;
+
+	portfolio: {
+		events: portfolioItem[];
+		programs: portfolioItem[];
+		projects: portfolioItem[];
+	};
+
+	users: shortMember[];
 
 	inn: string;
 	membersCount: number;
@@ -90,6 +104,37 @@ export class CompanyFormatter extends Formatter {
 				email: contents.email,
 				phone: contents.phone,
 				site: contents.site,
+
+				portfolio: {
+					events: (contents?.events || []).map((item) => ({
+						id: item.id,
+						title: item.title,
+					})),
+					projects: (contents?.projects || []).map((item) => ({
+						id: item.id,
+						title: item.title,
+					})),
+					programs: (contents?.programs || []).map((item) => ({
+						id: item.id,
+						title: item.title,
+					})),
+				},
+				users: contents.users.map((userItem) => {
+					const {
+						id,
+						photo,
+						profileTypes,
+						firstName,
+						surName,
+						middleName,
+					} = userItem;
+					return {
+						id,
+						name: `${surName} ${firstName} ${middleName || ""}`.trimEnd(),
+						photo: photo || pass,
+						role: profileTypes ? profileTypes[0]?.Name : "",
+					};
+				}),
 			};
 		});
 
